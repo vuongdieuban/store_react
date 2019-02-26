06ed45c8d867380923013e4dcd8bbda382b361dc
Ban Vuong <vuongdieuban@gmail.com>
2019-02-25 19:30:37 -0700

    build for production, remote server does not have enough RAM to build

---
+++ build/precache-manifest.5bed01dcc789a67cea7815c590ea4b14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "ea88fed00f952b152d75",
+    "url": "/static/css/main.8e874de6.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4f1cae815c742aea0094",
+    "url": "/static/js/1.4f1cae81.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea88fed00f952b152d75",
+    "url": "/static/js/main.ea88fed0.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "4f1cae815c742aea0094",
+    "url": "/static/css/1.eaf9b62a.chunk.css"
+  },
+  {
+    "revision": "120a711c5e179c804a23c0181204cd51",
+    "url": "/index.html"
+  }
+];
